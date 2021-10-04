e340b1db605738b5ce533bb3b7b92f6749ae2d62
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-04 11:15:59 +0200

    Initial Commit

---
+++ CoolCurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
