e08ecbade149cbdfa2554444af6067fe446ab346
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-05 08:23:48 +0200

    Setting up of ViewController

--- CoolCurrency/Controller/HomeCurrencyViewController.swift
+++ CoolCurrency/Controller/HomeCurrencyViewController.swift
@@ -7,10 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeCurrencyViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
