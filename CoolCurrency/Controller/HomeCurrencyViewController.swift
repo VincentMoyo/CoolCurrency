cca06fb88b7a717c17e63858ba86e3514a3d464a
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-08 17:38:29 +0200

    Modified pickerView again

--- CoolCurrency/Controller/HomeCurrencyViewController.swift
+++ CoolCurrency/Controller/HomeCurrencyViewController.swift
@@ -15,6 +15,29 @@ class HomeCurrencyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.fetchCurrencyList(for: "ZAR")
+        currencyPickerView.dataSource = self
+        currencyPickerView.delegate = self
+    }
+}
+
+extension HomeCurrencyViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        viewModel.currencyList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        Array(viewModel.currencyList.keys)[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = Array(viewModel.currencyList.keys)[row]
+        print(viewModel.convertCurrencyToCode(for: CurrencyCode(rawValue: selectedCurrency)!))
+        viewModel.fetchCurrencyList(for: viewModel.convertCurrencyToCode(for: CurrencyCode(rawValue: selectedCurrency)!))
     }
 }
 
@@ -22,5 +45,12 @@ extension HomeCurrencyViewController: CurrencyViewModelDelegate {
     
     func bindViewModel() {
         print(viewModel.currencyList)
+        viewModel.modelLoad = { result in
+            if result {
+                DispatchQueue.main.async {
+                    self.currencyPickerView.reloadAllComponents()
+                }
+            }
+        }
     }
 }
