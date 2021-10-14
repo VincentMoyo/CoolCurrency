3cc98e22f75d47c8e691a4d616ad32aa85742f90
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-14 21:26:53 +0200

    Created Convert View Controller

---
+++ CoolCurrency/Controller/CurrencyConversionViewController.h
@@ -0,0 +1,16 @@
+//
+//  CurrencyConversionViewController.h
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/14.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CurrencyConversionViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CoolCurrency/Controller/CurrencyConversionViewController.m
@@ -0,0 +1,31 @@
+//
+//  CurrencyConversionViewController.m
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/14.
+//
+
+#import "CurrencyConversionViewController.h"
+
+@interface CurrencyConversionViewController ()
+
+@end
+
+@implementation CurrencyConversionViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
