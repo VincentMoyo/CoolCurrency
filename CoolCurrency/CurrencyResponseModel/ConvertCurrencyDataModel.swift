9f360909e7e749d847fca64941cb962ab4361b90
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-15 16:05:38 +0200

    Added new data model

---
+++ CoolCurrency/CurrencyResponseModel/ConvertCurrencyDataModel.swift
@@ -0,0 +1,15 @@
+//
+//  ConvertCurrencyDataModel.swift
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/15.
+//
+
+import Foundation
+
+class ConvertCurrencyDataModel: NSObject {
+    @objc let primaryCurrency: Double = 0.0
+    @objc let primaryCurrencyName: String = ""
+    @objc let secondCurrency: Double = 0.0
+    @objc let secondCurrencyName: String = ""
+}
