733cc9a25d15f194aaf91f33c725849dfa9bd0be
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-10-15 16:13:28 +0200

    Added new objective C ViewModel

---
+++ CoolCurrency/CurrencyViewModel/ConvertCurrencyViewModel.h
@@ -0,0 +1,17 @@
+//
+//  ConvertCurrencyViewModel.h
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/15.
+//
+
+#import <Foundation/Foundation.h>
+#import "CoolCurrency-Swift.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConvertCurrencyViewModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CoolCurrency/CurrencyViewModel/ConvertCurrencyViewModel.m
@@ -0,0 +1,12 @@
+//
+//  ConvertCurrencyViewModel.m
+//  CoolCurrency
+//
+//  Created by Vincent Moyo on 2021/10/15.
+//
+
+#import "ConvertCurrencyViewModel.h"
+
+@implementation ConvertCurrencyViewModel
+
+@end
