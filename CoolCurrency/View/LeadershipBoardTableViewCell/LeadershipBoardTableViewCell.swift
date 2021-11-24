617ac6848c706c67cb0203033aae882ba6b70871
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-11-24 23:42:37 +0200

    made things private

--- CoolCurrency/View/LeadershipBoardTableViewCell/LeadershipBoardTableViewCell.swift
+++ CoolCurrency/View/LeadershipBoardTableViewCell/LeadershipBoardTableViewCell.swift
@@ -9,15 +9,12 @@ import UIKit
 
 class LeadershipBoardTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var positionNumber: UILabel!
-    @IBOutlet weak var userName: UILabel!
-    @IBOutlet weak var totalCorrectAnswers: UILabel!
-    @IBOutlet weak var totalScore: UILabel!
-    var counter = 0
+    @IBOutlet private weak var positionNumber: UILabel!
+    @IBOutlet private weak var userName: UILabel!
+    @IBOutlet private weak var totalCorrectAnswers: UILabel!
+    @IBOutlet private weak var totalScore: UILabel!
     
     func configure(with model: LeadershipBoardDataModel) {
-        counter += 1
-        positionNumber.text = "\(counter)."
         userName.text = model.name
         totalCorrectAnswers.text = model.correctAnswers
         totalScore.text = model.totalScore
